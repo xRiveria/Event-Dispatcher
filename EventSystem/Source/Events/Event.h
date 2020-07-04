2034150ee4653d3b9930a74a89b6936e487dc0f4
Ryan Tan <ryan-wende@outlook.com>
2020-07-05 01:57:23 +0800

    Add Input Polling Class & Base Events Setup

--- EventSystem/Source/Events/Event.h
+++ EventSystem/Source/Events/Event.h
@@ -3,16 +3,22 @@
 
 namespace EventSystem
 {
-	enum EventType
+#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::##type; }\
+								virtual EventType GetEventType() const override { return GetStaticType(); }\
+								virtual const char* GetName() const override { return #type; }
+
+#define EVENT_CLASS_CATEGORY(category) virtual int GetCategoryFlags() const override { return category; }
+	
+	enum class EventType
 	{
 		None = 0,
 		MouseScrolled = 1,
 		MouseMoved = 2,
 		MouseButtonPressed = 3,
 		MouseButtonReleased = 4,
-		KeyPressed,
-		KeyReleased,
-		KeyTyped
+		KeyPressed = 5,
+		KeyReleased = 6,
+		KeyTyped = 7
 	};
 
 	enum EventTypeCategory
@@ -20,13 +26,23 @@ namespace EventSystem
 		None = 0,
 		EventCategoryMouseEvent = 1,
 		EventCategoryKeyboardEvent = 2,
+		EventCategoryInputEvent = 3
 	};
 
 	class Event
 	{
 	public:
-		virtual EventType GetEventType() = 0;
+		bool hasEventBeenHandled = false;
+
+		virtual EventType GetEventType() const = 0;
 		virtual std::string ToString() const = 0;
+		virtual const char* GetName() const = 0;
+		virtual int GetCategoryFlags() const = 0;
+
+		inline bool IsInCategory(EventTypeCategory category)
+		{
+			return GetCategoryFlags() & category;
+		}
 	};
 }
 
--- EventSystem/Source/Events/KeyEvent.h
+++ EventSystem/Source/Events/KeyEvent.h
@@ -7,16 +7,30 @@ namespace EventSystem
 	class KeyEvent : public Event
 	{
 	public:
+		inline int GetKeyCode() const { return m_KeyCode; }
+		EVENT_CLASS_CATEGORY(EventCategoryInputEvent | EventCategoryKeyboardEvent)
 
 	protected:
-
 		KeyEvent(int keyCode) : m_KeyCode(keyCode) {}
 		int m_KeyCode;
 	};
 
 	class KeyPressedEvent : public KeyEvent
-	{
+	{	
+		KeyPressedEvent(int keyCode, int repeatCount) : KeyEvent(keyCode), m_TimesPressed(repeatCount) {}
+		inline int GetRepeatCount() const { return m_TimesPressed; }
 
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "Key Pressed Event: " << m_KeyCode << " Repeated For " << m_TimesPressed << " Times!";
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyPressed)
+
+	private:
+		int m_TimesPressed;
 	};
 
 	class KeyReleasedEvent : public KeyEvent
@@ -30,6 +44,8 @@ namespace EventSystem
 			ss << "Key Released Event: " << m_KeyCode;
 			return ss.str();
 		}
+
+		EVENT_CLASS_TYPE(KeyReleased);
 	};
 
 	class KeyTypedEvent : public KeyEvent
@@ -39,7 +55,11 @@ namespace EventSystem
 
 		std::string ToString() const override
 		{
-
+			std::stringstream ss; //Prints a string value.
+			ss << "Key Typed Event: " << m_KeyCode;
+			return ss.str();
 		}
+
+		EVENT_CLASS_TYPE(KeyTyped);
 	};
 }
